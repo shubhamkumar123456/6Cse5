1c39f87e75f5d47dc860aa5c69e7c2dea3c21ec8
shubham kumar <clboy768@gmail.com>
2025-12-27 10:19:29 +0530

    added mongodb commands

---
+++ mongoDb.js
@@ -0,0 +1,24 @@
+// Mongodb --> is a popular open source document oriented no- sql database. instead of using  tables and rows like traditional database , it uses JSON like object to store data
+
+// DataBase --> collection --> Document(data)
+
+// collection --> a collection is a grouping of mongodb documents it is equivalent of a table in relational database
+
+// Document --> a document is a record in mongodb collection and the basic uinit of data in mongodb
+
+
+// Commands -->
+
+//1) to view all database  -->   show dbs
+//2) Create new Database or use any existing Database -->  use DatabaseName
+//3) create a collection inside Database --> db.createCollection('collectionName')
+
+// 4) Insert Data inside Collection 
+        // a)insert Single Data --> db.collectionName.insertOne({key:values})
+        // b)insert Many Data --> db.collectionName.insertMany([{key:values} , {key:value}])
+
+// 5) get all data present inside a collection --> db.collectionName.find()
+// 6) get all filtered Data inside a collection-->  db.collectionName.find({key:value})
+
+//7) get a single data present inside collection -->  db.collectionName.findOne({key:value})
+        
